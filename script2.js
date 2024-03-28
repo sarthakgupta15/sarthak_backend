0b1fd0aa74bc8c0d484a72b8ae920d5acb2e03b7
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2024-03-28 19:35:44 +0530

    Express.js

--- script2.js
+++ script2.js
@@ -1,3 +1,37 @@
-var data = require("./script")
+// var data = require("./script")
 
-console.log(data);
+// console.log(data);
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('hi World')
+})
+
+app.get('/profile', function (req, res) {
+    res.send('hi from profile')
+})
+
+app.listen(3000)
+
+
+/*
+express framework for node.js
+express ka use case => routing
+routing =>
+https://www.facebook.com/profile/sarthak/aihfbafskbfafbksdn
+routes - /profile
+different url
+/profile = profile
+/contact = contact
+/like/afjjfs = like comment
+
+route types =>
+GET POST PUT PATCH DELETE
+Restful-api
+
+get route. can see data in url
+if entering password use POST route.
+
+*/
