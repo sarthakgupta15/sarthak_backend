d1944c5ee389c586bcc9b650741071193f97e275
sarthakgupta15 <sarthakgupta.univ@gmail.com>
2024-03-28 18:18:02 +0530

    Update

---
+++ script.js
@@ -0,0 +1,36 @@
+var a = 12;
+//console.log("Hello");
+//module.exports = a;
+
+// var oneLinerJoke = require('one-liner-joke');
+// console.log(oneLinerJoke.getRandomJoke());
+
+// var figlet = require("figlet");
+
+// figlet("Sarthak Is the Best !!", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+// figlet.text(
+//     "Boo!",
+//     {
+//       font: "Ghost",
+//       horizontalLayout: "default",
+//       verticalLayout: "default",
+//       width: 80,
+//       whitespaceBreak: true,
+//     },
+//     function (err, data) {
+//       if (err) {
+//         console.log("Something went wrong...");
+//         console.dir(err);
+//         return;
+//       }
+//       console.log(data);
+//     }
+//   );
